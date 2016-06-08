96cc5a2498d71fc8118c1f28c8b45fd319b6ba12
songjoonhyuk <songjoonhyuk@gmail.com>
2016-06-08 16:09:58 +0900

    login, logout base에 연결

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from accounts import views
 urlpatterns = [
 	url(r'^signup/$', views.signup, name='signup'),
 	url(r'^login/$', login, name='login'),
-	url(r'^login/$', logout, name='logout'),
+	url(r'^logout/$', logout, name='logout'),
 	url(r'^profile/$', views.profile, name='profile'),
 ]
