80ff119e23d8bec81fa457454d9a0bf75f053a70
songjoonhyuk <songjoonhyuk@gmail.com>
2016-06-08 15:35:27 +0900

    signup 기능 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def signup(request):
-	return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form,})
