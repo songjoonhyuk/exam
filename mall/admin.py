8013b23c1a93f8d909d99f1e7ceb8d86238587ae
songjoonhyuk <songjoonhyuk@gmail.com>
2016-06-08 16:40:46 +0900

    index페이지에모델 리스트 목록 추가

--- mall/admin.py
+++ mall/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from mall.models import Category, Shop, Review
 # Register your models here.
+
+admin.site.register(Category)
+
+admin.site.register(Shop)
+
+admin.site.register(Review)
--- mall/views.py
+++ mall/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Category, Shop, Review
 
 # Create your views here.
 
 def index(request):
-	return render(request, 'mall/index.html')
+	category_list = Category.objects.all()
+	shop_list = Shop.objects.all()
+	review_list = Review.objects.all()
+	return render(request, 'mall/index.html', {
+		'shop_list':shop_list,
+		'category_list':category_list,
+		'review_list':review_list,
+		})
